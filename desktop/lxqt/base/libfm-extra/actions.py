4467e62e53ca84eda7c18ce51352865048bbd1c2
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2015-08-03 09:03:17 +0300

    libfm-extra:fixed build dep

---
+++ desktop/lxqt/base/libfm-extra/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+
+def setup():
+    shelltools.system("./autogen.sh")
+    autotools.configure("--sysconfdir=/etc \
+                         --with-extra-only \
+                         --with-gtk=no     \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "COPYING")

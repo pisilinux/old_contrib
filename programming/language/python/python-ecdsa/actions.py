eec3e6d595dd2de398982805c0ba12ce893baf4b
ibrahimkaraguzel <ibrahimkaraguzel@icloud.com>
2016-04-01 04:02:29 +0300

    python-ecdsa:first pisi release,built with docker

---
+++ programming/language/python/python-ecdsa/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir="python-ecdsa-python-ecdsa-%s" % get.srcVERSION()
+
+def setup():
+    shelltools.cd("..")
+    shelltools.makedirs("build_python3")
+    shelltools.copytree("./%s" % WorkDir,  "build_python3")
+    shelltools.system("sed -ie 's#/usr/bin/env python#/usr/bin/env python3#' python-ecdsa-python-ecdsa-0.13/ecdsa/*.py")
+    shelltools.cd(WorkDir)
+
+def build():
+    pythonmodules.compile()
+
+    shelltools.cd("../build_python3/%s" % WorkDir)
+    pythonmodules.compile(pyVer="3.4")
+
+def install():
+    pythonmodules.install()
+    
+    shelltools.cd("../build_python3/%s" % WorkDir)
+    pythonmodules.install(pyVer="3.4")

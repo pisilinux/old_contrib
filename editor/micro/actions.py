cb1e286ef4b1c4547059a9fa88f975fecec1c45c
Erdem Ersoy <erdemersoy@live.com>
2016-11-29 17:43:29 +0300

    Some modifications to actions.py of micro.

--- editor/micro/actions.py
+++ editor/micro/actions.py
@@ -9,14 +9,15 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+shelltools.export("GOPATH", get.workDIR())
+shelltools.export("GOBIN", "%s/usr/bin" % get.installDIR())
+
 def setup():
     shelltools.move("%s/micro-1.1.2" % get.workDIR(), "%s/src/github.com/zyedidia/micro" % get.workDIR())
 
 def install():
     shelltools.cd("%s/src/github.com/zyedidia/micro" % get.workDIR())
-    shelltools.export("GOPATH", get.workDIR())
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dobin("%s/bin/micro" % get.workDIR())
+    autotools.install()
     pisitools.dodoc("%s/src/github.com/zyedidia/micro/LICENSE" % get.workDIR(), "%s/src/github.com/zyedidia/micro/README.md" % get.workDIR())
 
 

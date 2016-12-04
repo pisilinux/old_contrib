a1a3d5b4ed154c9a68f1d5664be8ca24a4cabe5b
Erdem Ersoy <erdemersoy@live.com>
2016-12-04 19:49:13 +0300

    DOSBox introduced to Pisi Linux. :)

---
+++ hardware/emulator/dosbox/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--prefix=/usr \
+                                  --sysconfdir=/etc/dosbox")
+
+def install():
+    autotools.rawInstall("DESTDIR='%s'" % get.installDIR())

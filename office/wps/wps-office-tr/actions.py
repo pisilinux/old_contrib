5f5da6b1b8e360cea0930e1fc6a4aead1508a9f2
aligulle1 <muratalgul1978@outlook.com>
2017-06-14 13:51:09 +0300

    wps

---
+++ office/wps/wps-office-tr/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+
+from pisi.actionsapi import pisitools
+
+# if pisi can't find source directory, see /var/pisi/wps-office-tr/work/ and:
+WorkDir="."
+
+
+
+def install():
+    pisitools.insinto("opt/kingsoft/wps-office/office6/mui/tr_TR","tr_TR/*")
+

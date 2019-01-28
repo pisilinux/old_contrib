f3bba6fe3e69b203bf641327729a36eb0e902abe
Rmys <muscnsl@gmail.com>
2019-01-28 14:58:12 +0300

    python3-shiboken2

--- programming/language/python/python3-shiboken2/actions.py
+++ programming/language/python/python3-shiboken2/actions.py
@@ -10,26 +10,23 @@ from pisi.actionsapi import get
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import shelltools
 
-WorkDir = "shiboken2-master"
+#WorkDir = "shiboken2-master"
 
 def setup():
+    shelltools.cd("sources/shiboken2")
     shelltools.makedirs("build3")
     shelltools.cd("build3")
-    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr  \
-                        -DCMAKE_INSTALL_SYSCONFDIR=/etc \
-                        -DUSE_PYTHON_VERSION=3.4 \
-                        -DBUILD_TESTS=OFF \
-                        -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.4m \
-                        -DPYTHON_LIBRARY=/usr/lib/libpython3.4m.so \
-                        -DCMAKE_INSTALL_LIBDIR=/usr/lib", sourceDir="..")
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                          -DBUILD_TESTS=OFF \
+                          -DUSE_PYTHON_VERSION=3", sourceDir="..")
 
 
 def build():
-    shelltools.cd("build3")
+    shelltools.cd("sources/shiboken2/build3")
     autotools.make()
 
 def install():
-    shelltools.cd("build3")
+    shelltools.cd("sources/shiboken2/build3")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
 

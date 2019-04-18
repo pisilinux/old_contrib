4ad4ee9ee45f1649a554a8a77a1af4e6e25806c2
4fury <lilian2water@gmail.com>
2019-04-18 22:29:29 +0300

    .

--- network/download/aria2/actions.py
+++ network/download/aria2/actions.py
@@ -4,7 +4,6 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 

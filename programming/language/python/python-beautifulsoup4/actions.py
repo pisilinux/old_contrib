7ab10da7e4065652606d961a4cc9948e96105642
4fury <lilian2water@gmail.com>
2019-04-26 07:11:06 +0300

    beautifulsoup4: version bump

--- programming/language/python/python-beautifulsoup4/actions.py
+++ programming/language/python/python-beautifulsoup4/actions.py
@@ -14,18 +14,21 @@ WorkDir="beautifulsoup4-%s" % get.srcVERSION()
 def setup():
     shelltools.cd("..")
     shelltools.makedirs("build_python3")
-    shelltools.copytree("./%s" % WorkDir,  "build_python3")
+    shelltools.copytree("./%s" % WorkDir, "build_python3")
     shelltools.cd(WorkDir)
 
 def build():
     pythonmodules.compile()
 
     shelltools.cd("../build_python3/%s" % WorkDir)
-    pythonmodules.compile(pyVer="3")
+    pythonmodules.compile(pyVer = "3")
 
 def install():
     pythonmodules.install("--optimize=1 --skip-build")
     
     shelltools.cd("../build_python3/%s" % WorkDir)
-    pythonmodules.install(pyVer="3")
-    pisitools.dodoc("AUTHORS*", "COPYING*", "NEWS*", "README*")
+    pythonmodules.install(pyVer = "3")
+    pisitools.dodoc("COPYING.txt", \
+    "LICENSE", \
+    "NEWS.txt", \
+    "README.md")

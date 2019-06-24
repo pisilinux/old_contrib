c524b1d0454cb89aee62f9caf6e63e4d4aa3f854
4fury <lilian2water@gmail.com>
2019-06-24 20:27:11 +0300

    opus: add. tools

---
+++ multimedia/sound/opus-tools/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    autotools.configure("--enable-sse \
+    --enable-assertions \
+    --disable-static \
+    --disable-oggtest \
+    --disable-opustest \
+    --disable-opusfiletest \
+    --disable-libopusenctest")
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING")

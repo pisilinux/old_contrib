170bb888293ee2451c789156735bc9f3b9033603
4fury <lilian2water@gmail.com>
2019-07-04 20:15:44 +0300

    f2fs-tools

---
+++ hardware/disk/f2fs-tools/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+def setup():
+	shelltools.system("sed -i '16,17d' tools/Makefile.am")
+	shelltools.system("./autogen.sh")
+	autotools.configure("--without-selinux")
+
+def build():
+	autotools.make()
+
+def install():
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+	pisitools.dodoc("COPYING", "README")

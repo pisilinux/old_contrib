5a85d1253a1db399d8ff018a43fb002538dad7e6
4fury <lilian2water@gmail.com>
2019-10-27 17:59:37 +0300

    xfce4-dict update.

--- desktop/xfce/addon/xfce4-dict/actions.py
+++ desktop/xfce/addon/xfce4-dict/actions.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
@@ -21,5 +21,5 @@ def build():
 def install():
 	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-	pisitools.dodoc("AUTHORS", "ChangeLog*", "NEWS", "README")
+	pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
 

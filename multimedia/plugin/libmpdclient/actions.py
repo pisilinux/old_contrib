4696a6d2727c2272c48034648e4f638b902781cf
4fury <lilian2water@gmail.com>
2019-10-28 22:51:18 +0300

    mpd.

---
+++ multimedia/plugin/libmpdclient/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+	shelltools.system("meson -Dtcp=true --prefix=/usr build")
+
+def build():
+	shelltools.system("ninja -C build")
+
+def install():
+	shelltools.system("DESTDIR=%s ninja -C build install" % get.installDIR())
+
+	pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README.rst")

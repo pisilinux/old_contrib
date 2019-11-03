b85e13bcb638d55178c982f8418342b01451e2a1
4fury <lilian2water@gmail.com>
2019-11-03 15:24:47 +0300

    bump, clean, wnck-pager was spoiled in lounge-night theme :(

--- util/archive/zziplib/actions.py
+++ util/archive/zziplib/actions.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
@@ -10,21 +10,20 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-vif")
-    autotools.configure("--disable-static \
-                         --enable-sdl \
-                         --with-zlib")
+#	autotools.autoreconf("-vif")
+	autotools.configure("--disable-static --enable-sdl --with-zlib")
 
 def build():
-    autotools.make()
+	autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    # Extract and install shipped man pages
-    shelltools.system("tar xvf docs/zziplib-manpages.tar")
-    for manp in shelltools.ls("man3"):
-        pisitools.doman("man3/%s" % manp)
+	# Extract and install shipped man pages
+	shelltools.system("tar xvf docs/zziplib-manpages.tar")
+	for manp in shelltools.ls("man3"):
+		pisitools.doman("man3/%s" % manp)
 
-    pisitools.dohtml("docs/*.htm*")
-    pisitools.dodoc("ChangeLog", "COPYING.LIB", "README", "TODO", "docs/COPYING*", "docs/README.SDL")
+	pisitools.dohtml("docs/*.htm*")
+	pisitools.dodoc("ChangeLog", "COPYING.LIB", "README", "TODO", "docs/COPYING*", "docs/README.SDL")
+

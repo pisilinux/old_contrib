e779d2a70df9adf04265a69c7e52f3901becaca5
4fury <lilian2water@gmail.com>
2019-11-20 17:21:59 +0300

    bump.

--- desktop/xfce/base/xfce4-session/actions.py
+++ desktop/xfce/base/xfce4-session/actions.py
@@ -1,20 +1,18 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-#from pisi.actionsapi import shelltools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-	autotools.configure("\
-	--prefix=/usr \
-	--enable-polkit \
-	--disable-static \
-	--disable-legacy-sm \
-	--with-x")
+	# /etc/env.d/03locale
+	shelltools.system("sed -i '21aunset LC_ALL\\n' scripts/startxfce4.in")
+	autotools.configure("--enable-polkit --disable-static --disable-legacy-sm --with-x")
 
 	#pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
 	#pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
@@ -24,12 +22,6 @@ def build():
 	autotools.make()
 
 def install():
-#	shelltools.system("sed -i '102s/\<exec\>/& dbus-launch --exit-with-session/' scripts/xinitrc")
 	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-	pisitools.dodoc("AUTHORS", \
-	"COPYING", \
-	"BUGS", \
-	"NEWS", \
-	"README", \
-	"TODO")
+	pisitools.dodoc("AUTHORS", "COPYING", "BUGS", "NEWS", "README", "TODO")

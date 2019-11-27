a3c8c48cea615ee2618b0e3671e368e44472043b
blue-devil <bluedevil@sctzine.com>
2019-11-27 03:34:52 +0000

    yara and python bindings: version bump

--- programming/language/python/yara/actions.py
+++ programming/language/python/yara/actions.py
@@ -4,13 +4,10 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
-
-WorkDir="yara-%s" % get.srcVERSION()
 
 def setup():
     autotools.autoreconf("-vfi")
@@ -19,15 +16,6 @@ def setup():
 def build():
     autotools.make()
 
-    shelltools.cd("yara-python")
-    pythonmodules.compile(pyVer="2.7")
-    pythonmodules.compile(pyVer="3")
-
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "CONTRIBUTORS", "README*", "COPYING")
-    pisitools.insinto("/usr/share/doc/yara/", "docs/")
-
-    shelltools.cd("yara-python")
-    pythonmodules.install(pyVer="2.7")
-    pythonmodules.install(pyVer="3")
+    pisitools.dodoc("README*", "COPYING")

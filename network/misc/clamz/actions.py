f9d096160e0935ea28132fe2ba00f2247010b22b
blue-devil <bluedevil@sctzine.com>
2019-12-03 19:04:47 +0000

    clamz : rebuilt

--- network/misc/clamz/actions.py
+++ network/misc/clamz/actions.py
@@ -4,6 +4,7 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
@@ -12,11 +13,12 @@ def setup():
     autotools.configure()
 
 def build():
+    pisitools.dosed("Makefile","LDFLAGS = ", "LDFLAGS = -Wl,-O1,--as-needed ")
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     
     #pisitools.remove("/usr/share/applications/mimeinfo.cache")
-
+    pisitools.insinto("/usr/share/applications", "clamz.desktop")
     pisitools.dodoc("COPYING", "README")

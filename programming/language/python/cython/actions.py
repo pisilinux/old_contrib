a013e6a5154fcdc583d279dd1d1bc901f90245ec
blue-devil <bluedevil@sctzine.com>
2019-12-06 03:58:33 +0000

    cython : split packages

--- programming/language/python/cython/actions.py
+++ programming/language/python/cython/actions.py
@@ -9,27 +9,17 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "cython-%s" % get.srcVERSION()
-
-def setup():
-    shelltools.cd("..")
-    shelltools.makedirs("build_python3")
-    shelltools.copytree("./%s" % WorkDir,  "build_python3")
-    shelltools.cd(WorkDir)
-
 def build():
     shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -Wl,-O1 -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--sort-common")
     shelltools.export("CFLAGS", "-fno-strict-aliasing -mtune=generic -march=x86-64 -O2 -pipe -fstack-protector -D_FORTIFY_SOURCE=2 -g -fPIC -fwrapv -DNDEBUG -g -fwrapv -O3 -Wno-strict-aliasing")
     pythonmodules.compile()
-   
-    shelltools.cd("../build_python3/%s" % WorkDir)
-    pythonmodules.compile(pyVer="3")
+
+# it takes hours to run test, run with caution!
+#def check():
+#    pythonmodules.run("runtests.py -vv --no-pyregr")
 
 def install():
     pythonmodules.install()
-   
-    shelltools.cd("../build_python3/%s" % WorkDir)
-    pythonmodules.install(pyVer="3")
    
     for dirs in ["Doc", "Demos", "tests"]:
         pisitools.insinto("%s/%s" % (get.docDIR(), get.srcNAME()), dirs)

c2216991359ac0a2698f0e68f9d3f066ae97f521
blue-devil <bluedevil@sctzine.com>
2019-12-06 14:49:22 +0000

    python3-iniconfig

---
+++ programming/language/python3/python3-iniconfig/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def build():
+    shelltools.export("SETUPTOOLS_SCM_PRETEND_VERSION","1.0.0")
+    pythonmodules.compile(pyVer="3")
+
+def install():
+    shelltools.export("SETUPTOOLS_SCM_PRETEND_VERSION","1.0.0")
+    pythonmodules.install(pyVer="3")
+

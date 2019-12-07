d9d381000599ecaddfc115a4815e0f98594f02a6
blue-devil <bluedevil@sctzine.com>
2019-12-07 06:46:41 +0000

    pyelftools : ver bump and rebuilt

--- programming/language/python3/python3-pyelftools/actions.py
+++ programming/language/python3/python3-pyelftools/actions.py
@@ -11,6 +11,10 @@ from pisi.actionsapi import get
 
 def build():
     pythonmodules.compile(pyVer="3")
+    
+def check():
+    pythonmodules.run("test/run_all_unittests.py",pyVer="3")
+    pythonmodules.run("test/run_examples_test.py",pyVer="3")
 
 def install():
     pythonmodules.install(pyVer="3")

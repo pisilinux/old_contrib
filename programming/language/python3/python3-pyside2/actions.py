b912729ad541ff6788140b8fe6bc0dbb2ab4a312
blue-devil <bluedevil@sctzine.com>
2019-12-12 00:10:33 +0000

    pyside2 : version bump and moved to py3 folder

--- programming/language/python3/python3-pyside2/actions.py
+++ programming/language/python3/python3-pyside2/actions.py
@@ -4,9 +4,9 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import shelltools
 
@@ -16,9 +16,11 @@ def setup():
     shelltools.cd("sources/pyside2")
     shelltools.makedirs("build3")
     shelltools.cd("build3")
-    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+    #shelltools.export("CXX","/usr/bin/clang++")
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr\
+                          -DCMAKE_BUILD_TYPE=Release \
                           -DBUILD_TESTS=OFF \
-                          -DUSE_PYTHON_VERSION=3", sourceDir="..")
+                          -DPYTHON_EXECUTABLE=/usr/bin/python3", sourceDir="..")
 
 def build():
     shelltools.cd("sources/pyside2/build3")

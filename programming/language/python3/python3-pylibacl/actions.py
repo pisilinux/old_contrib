cbc9da96e3dfe5275a45f441fd7129539d2ba3e4
blue-devil <bluedevil@sctzine.com>
2019-12-13 23:58:35 +0000

    python3-pylibacl split packages and rebuilt

---
+++ programming/language/python3/python3-pylibacl/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def build():
+    shelltools.export("CFLAGS", "-Wno-strict-aliasing %s" % get.CFLAGS())
+    shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -Wl,-O1 -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--sort-common")
+    pythonmodules.compile(pyVer="3")
+
+def install():
+    pythonmodules.install(pyVer="3")

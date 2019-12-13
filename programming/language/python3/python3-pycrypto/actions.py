2e180d8fc4d8c0d3db1ff729224587844c3ec9cf
blue-devil <bluedevil@sctzine.com>
2019-12-13 23:50:27 +0000

    python3-pycrypto split packages and rebuilt

---
+++ programming/language/python3/python3-pycrypto/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+
+def build():
+    shelltools.export("CFLAGS", "-fno-strict-aliasing -Wno-discarded-array-qualifiers -Wno-unused-const-variable -Wno-bool-compare -Wno-tautological-compare")
+    shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -Wl,-O1 -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--sort-common")
+    pythonmodules.compile(pyVer="3")
+
+def install():
+    pythonmodules.install(pyVer="3")

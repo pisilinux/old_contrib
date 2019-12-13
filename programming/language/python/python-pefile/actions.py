53829431975af6922eb6b4535171aa7d923b792f
blue-devil <bluedevil@sctzine.com>
2019-12-13 23:02:04 +0000

    python-pefile split packages

--- programming/language/python/python-pefile/actions.py
+++ programming/language/python/python-pefile/actions.py
@@ -5,34 +5,12 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
-
-WorkDir="pefile-%s" % get.srcVERSION()
-
-def setup():
-    shelltools.cd("..")
-    shelltools.makedirs("build_python3")
-    shelltools.copytree("./%s" % WorkDir,  "build_python3")
-    shelltools.cd(WorkDir)
 
 def build():
     pythonmodules.compile()
 
-    shelltools.cd("../build_python3/%s" % WorkDir)
-    pythonmodules.compile(pyVer="3")
-
 def check():
     pythonmodules.compile("check")
-    
-    shelltools.cd("../build_python3/%s" % WorkDir)
-    shelltools.system("python3 setup.py check")
 
 def install():
     pythonmodules.install()
-    
-    shelltools.cd("../build_python3/%s" % WorkDir)
-    pythonmodules.install(pyVer="3")
-
-    pisitools.dodoc("LICENSE", "MANIFEST*", "README*")

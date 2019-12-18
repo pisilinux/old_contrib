126d55e955b1566f422dadcd198f8560e27d2940
blue-devil <bluedevil@sctzine.com>
2019-12-18 21:14:45 +0000

    pyspotify for py3.8 added : first release

---
+++ programming/language/python3/python3-pyspotify/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def build():
+    shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -Wl,-O1 -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--sort-common")
+    pythonmodules.compile(pyVer="3")
+
+def install():
+    pythonmodules.install(pyVer="3")
+
+    for dirs in ["examples"]:
+        pisitools.insinto("%s/%s" % (get.docDIR(), get.srcNAME()), dirs)

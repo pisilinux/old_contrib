6f59425d82457823432ff60c847b3862de425eb1
blue-devil <bluedevil@sctzine.com>
2019-12-23 18:23:42 +0000

    aircrack-ng : version bump

--- network/analyzer/aircrack-ng/actions.py
+++ network/analyzer/aircrack-ng/actions.py
@@ -3,12 +3,22 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.export("CFLAGS", "%s -Wno-strict-overflow" % get.CFLAGS())
+    autotools.autoreconf("-i")
+    autotools.configure("--with-experimental")
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
-    autotools.make("sqlite=true experimental=true pcre=true")
+    autotools.make()
+    
+def check():
+    autotools.make("check")
 
 def install():
     autotools.install("sqlite=true experimental=true pcre=true")

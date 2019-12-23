aab8c9b7bcc90ce468d3967728eed3640e71e88d
blue-devil <bluedevil@sctzine.com>
2019-12-23 18:27:15 +0000

    python3-shiboken2 : version bump

--- programming/language/python3/python3-shiboken2/actions.py
+++ programming/language/python3/python3-shiboken2/actions.py
@@ -4,29 +4,24 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import shelltools
-
-#WorkDir = "shiboken2-master"
 
 def setup():
     shelltools.cd("sources/shiboken2")
     shelltools.makedirs("build3")
     shelltools.cd("build3")
     cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                          -DCMAKE_BUILD_TYPE=Release \
                           -DBUILD_TESTS=OFF \
                           -DUSE_PYTHON_VERSION=3", sourceDir="..")
 
 
 def build():
     shelltools.cd("sources/shiboken2/build3")
-    autotools.make()
+    cmaketools.make()
 
 def install():
     shelltools.cd("sources/shiboken2/build3")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-

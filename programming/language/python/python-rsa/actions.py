45dd89904ca836a5f8e3a057e089267fc8dce7cd
blue-devil <bluedevil@sctzine.com>
2019-12-28 22:56:16 +0000

    python-rsa : fix

--- programming/language/python/python-rsa/actions.py
+++ programming/language/python/python-rsa/actions.py
@@ -5,9 +5,16 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
 
 def build():
     pythonmodules.compile()
 
 def install():
     pythonmodules.install()
+    pisitools.rename("/usr/bin/pyrsa-verify","pyrsa-verify-py2")
+    pisitools.rename("/usr/bin/pyrsa-keygen","pyrsa-keygen-py2")
+    pisitools.rename("/usr/bin/pyrsa-priv2pub","pyrsa-priv2pub-py2")
+    pisitools.rename("/usr/bin/pyrsa-sign","pyrsa-sign-py2")
+    pisitools.rename("/usr/bin/pyrsa-encrypt","pyrsa-encrypt-py2")
+    pisitools.rename("/usr/bin/pyrsa-decrypt","pyrsa-decrypt-py2")

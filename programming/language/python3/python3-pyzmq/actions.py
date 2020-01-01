8a9c63c935f0455b33d1cca953b5950e37dd0cbb
blue-devil <bluedevil@sctzine.com>
2020-01-01 11:07:01 +0000

    python3-pyzmq:first release

---
+++ programming/language/python3/python3-pyzmq/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def build():
+    shelltools.export("CFLAGS", "-Wno-implicit-function-declaration -Wno-unused-but-set-variable -Wno-nonnull -Wno-tautological-compare -Wno-deprecated-declarations %s" % get.CFLAGS())
+    shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread")
+    pythonmodules.compile(pyVer="3")
+    
+    #pythonmodules.run("setup.py build_sphinx")
+    
+#def check():
+#    pythonmodules.compile("test")
+
+def install():
+    pisitools.cflags.add("-Wno-implicit-function-declaration")
+    pythonmodules.install(pyVer="3")

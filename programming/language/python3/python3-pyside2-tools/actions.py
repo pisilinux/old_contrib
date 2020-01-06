e0819251b77b6a22dd65aee7dd2c98028886c3aa
blue-devil <bluedevil@sctzine.com>
2020-01-06 20:20:33 +0000

    python3-pyside2-tools:version bump

--- programming/language/python3/python3-pyside2-tools/actions.py
+++ programming/language/python3/python3-pyside2-tools/actions.py
@@ -4,11 +4,11 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import shelltools
 
 #WorkDir = "pyside2-master"
 

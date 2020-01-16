ecd9ce5182b6dbfcbef53548c20e6b3f18d1a006
4fury <lilian2water@gmail.com>
2020-01-16 10:04:51 +0300

    bump.

--- desktop/xfce/addon/xfce4-panel-profiles/actions.py
+++ desktop/xfce/addon/xfce4-panel-profiles/actions.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
@@ -17,5 +17,5 @@ def build():
 def install():
 	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-	pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
+	#pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
 

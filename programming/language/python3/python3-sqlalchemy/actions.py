e95c207e4f5017850851245c50a5c10ab900953b
blue-devil <bluedevil@sctzine.com>
2020-01-24 01:42:28 +0300

    python3-sqlalchemy:ver bump

--- programming/language/python3/python3-sqlalchemy/actions.py
+++ programming/language/python3/python3-sqlalchemy/actions.py
@@ -9,6 +9,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def build():
+    # fıx unused direct dependency analysis
     shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -Wl,-O1 -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--sort-common")
     pythonmodules.compile(pyVer="3")
     

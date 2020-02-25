493905b3e45d271758ec7d356e97760c4fd20d7d
blue-devil <bluedevil@sctzine.com>
2020-02-25 04:29:29 +0300

    python-mock:version bump

--- programming/language/python/python-mock/actions.py
+++ programming/language/python/python-mock/actions.py
@@ -5,20 +5,9 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
-
-def setup():
-    shelltools.system("rm %s/mock-3.0.5/mock/tests/testhelpers_py3.py" % get.workDIR())
 
 def build():
     pythonmodules.compile()
-    
-    #pythonmodules.run("setup.py build_sphinx")
-    
-def check():
-    pythonmodules.compile("test")
 
 def install():
     pythonmodules.install()

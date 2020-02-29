e8b7cdab92f391ff18e260d187103c8e2ef812e8
4fury <lilian2water@gmail.com>
2020-02-29 04:19:15 +0300

    version fix.

--- game/misc/plib/actions.py
+++ game/misc/plib/actions.py
@@ -2,19 +2,17 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-
 def setup():
-    shelltools.export("CXXFLAGS", "%s -fPIC" % get.CFLAGS())
+    pisitools.cxxflags.add("-fPIC")
     autotools.autoreconf("-vif")
     autotools.configure("--disable-static")
-    
+
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():

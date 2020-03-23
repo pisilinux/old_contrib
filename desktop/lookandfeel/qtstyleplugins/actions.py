2f730f06543037771e924d3de4ad4587194e6b2a
4fury <lilian2water@gmail.com>
2020-03-23 21:24:53 +0300

    fix.

--- desktop/lookandfeel/qtstyleplugins/actions.py
+++ desktop/lookandfeel/qtstyleplugins/actions.py
@@ -4,16 +4,13 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import qt5
 from pisi.actionsapi import get
 
-def setup():
-	shelltools.export("CXXFLAGS", \
-	"%s -Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-unused-result" % \
-	get.CXXFLAGS())
+flg = "-Wno-deprecated-declarations -Wno-implicit-fallthrough -Wno-unused-result"
 
-	qt5.configure()
+def setup():
+	qt5.configure(parameters = "QMAKE_CXXFLAGS+='%s'" % flg)
 
 def build():
 	qt5.make()

250de83f25200ea0ae5b936a95104628edafa823
4fury <lilian2water@gmail.com>
2020-04-17 16:53:26 +0300

    sK1: new packages.

---
+++ multimedia/graphics/sk1/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+
+def setup():
+	shelltools.system("mv setup-sk1.py setup.py")
+
+def build():
+	pythonmodules.compile()
+
+def install():
+	pythonmodules.install()
+

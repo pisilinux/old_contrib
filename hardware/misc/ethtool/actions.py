caa89b93bca5abb0a89d6ad3475982283e0aa1a8
blue-devil <bluedevil@sctzine.com>
2020-04-28 23:29:49 +0300

    ethtool:ver.bump

--- hardware/misc/ethtool/actions.py
+++ hardware/misc/ethtool/actions.py
@@ -3,9 +3,9 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
     autotools.configure()
@@ -15,4 +15,4 @@ def build():
 
 def install():
     autotools.rawInstall('DESTDIR="%s" INSTALL="install -p"' % get.installDIR())
-    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "COPYING", "LICENSE")
+    pisitools.dodoc("README", "COPYING", "LICENSE")

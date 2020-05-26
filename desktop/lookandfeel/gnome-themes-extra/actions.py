8c5de74f718eb8ef57330820e0817e84ec151980
4fury <lilian2water@gmail.com>
2020-05-26 19:59:54 +0300

    gnome-themes-extra rebuild.

--- desktop/lookandfeel/gnome-themes-extra/actions.py
+++ desktop/lookandfeel/gnome-themes-extra/actions.py
@@ -9,19 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-w = "%s/%s" % (get.workDIR(), get.srcDIR())
-t = "./themes/*/gtk-2.0"
-
 def setup():
-	pisitools.dosed("themes/Adwaita-dark/gtk-2.0/hacks-dark.rc", "000000", "777777")
-	shelltools.system("sed -i '82a\ \ GtkMenuBar::window-dragging\ =\ 1' %s/main.rc" % t)
-	shelltools.system("sed -i '83a\ \ GtkToolbar::window-dragging\ =\ 1' %s/main.rc" % t)
-
-	shelltools.unlink("%s/assets/*" % t)
-	shelltools.copy("themes/Adwaita/gtk-2.0/assets-external.svg", "themes/Adwaita-dark/gtk-2.0/")
-	for i in ["themes/Adwaita/gtk-2.0", "themes/Adwaita-dark/gtk-2.0"]:
-		shelltools.system("cd %s; ./render-assets.sh; ./render-assets-external.sh; cd %s" % (i, w))
-
 	shelltools.system("./autogen.sh --prefix=/usr")
 
 def build():

32aca322a417d40789c2d3b46c319db5df173fe5
4fury-c3440d8 <icu7c1@gmail.com>
2020-09-22 19:34:55 +0300

    cutegram.

--- hardware/mobile/cutegram/actions.py
+++ hardware/mobile/cutegram/actions.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import qt5
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import qt5
 
-WorkDir = "Cutegram-2.7.0-stable/Cutegram"
+WorkDir = "Cutegram-2.7.1-stable/Cutegram"
 
 def setup():
-    shelltools.system("qmake-qt5 -r .. PREFIX=/usr")
+    shelltools.system("qmake -r .. PREFIX=/usr")
     qt5.configure()
 
 def build():

5ab6115f367654be29c3df3318ccb7fcf0eae6b4
4fury-c3440d8 <uglyside@yandex.ru>
2021-06-24 20:56:35 +0300

    obconf rebuild.

--- x11/wm/obconf/actions.py
+++ x11/wm/obconf/actions.py
@@ -2,22 +2,18 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
-
+from pisi.actionsapi import shelltools, autotools, pisitools
 
 def setup():
-    autotools.configure("--disable-static \
-                         --enable-nls")
+	shelltools.system("./bootstrap")
+	autotools.configure()
 
 def build():
-    autotools.make()
+	autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+	autotools.install()
 
-    pisitools.dodoc("AUTHORS", "CHANGELOG", "COPYING", "NEWS", "README", "TODO")
+	pisitools.dodoc("AUTHORS")

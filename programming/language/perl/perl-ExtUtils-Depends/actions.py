9f27aa9774178a149aa77adffb08c53d9e756179
4fury-c3440d8 <uglyside@yandex.ru>
2021-09-19 06:05:55 +0300

    perl-ExtUtils-Depends.

--- programming/language/perl/perl-ExtUtils-Depends/actions.py
+++ programming/language/perl/perl-ExtUtils-Depends/actions.py
@@ -5,10 +5,7 @@
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import perlmodules
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
-
-WorkDir = "ExtUtils-Depends-%s" % get.srcVERSION()
 
 def setup():
 	perlmodules.configure()

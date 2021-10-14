bc5134c1631e9fe016b8d201e70136966b5c8256
4fury-c3440d8 <uglyside@yandex.ru>
2021-10-14 19:09:14 +0300

    moved to main.

--- network/mail/spamassassin/comar/package.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import os
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    os.system("sa-update")
-    os.system("/bin/chmod 755 /var/lib/spamd")
-    os.system("/bin/chown spamd: /var/lib/spamd -R")

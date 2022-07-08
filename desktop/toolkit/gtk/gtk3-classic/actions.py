be201f88dfc5071c160ea245b31398fbeb1d81a4
4fury-c3440d8 <uglyside@yandex.ru>
2022-07-08 18:15:59 +0300

    clean up.

--- desktop/toolkit/gtk/gtk3-classic/actions.py
+++ desktop/toolkit/gtk/gtk3-classic/actions.py
@@ -4,24 +4,22 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import mesontools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools, mesontools, pisitools, get
 
 j = ''.join([
-    ' -Ddemos=true',
+    ' -Dx11_backend=true',
+    ' -Dwayland_backend=true',
+    ' -Dbroadway_backend=true',
+    ' -Dcolord=no',
+    ' -Ddemos=false',
     ' -Dtests=false',
     ' -Dexamples=false',
-    ' -Dcolord=no',
-    ' -Dquartz_backend=false',
     ' -Dwin32_backend=false',
-    ' -Dbroadway_backend=true ',
+    ' -Dquartz_backend=false '
     ])
 
 def setup():
 	shelltools.export("CFLAGS", get.CFLAGS().replace("-fomit-frame-pointer",""))
-
 	mesontools.configure(j)
 
 def build():
@@ -29,6 +27,4 @@ def build():
 
 def install():
 	mesontools.install()
-
 	pisitools.rename("/usr/bin/gtk-update-icon-cache", "gtk3-update-icon-cache")
-

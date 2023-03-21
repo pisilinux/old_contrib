b2afcdbea6ac574caeb702583eeb76bd60076619
4fury-c3440d8 <uglyside@yandex.ru>
2023-03-21 20:05:12 +0300

    enlightenment.

--- desktop/enlightenment/base/enlightenment/actions.py
+++ desktop/enlightenment/base/enlightenment/actions.py
@@ -4,8 +4,7 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import mesontools
-from pisi.actionsapi import pisitools
+from pisi.actionsapi import mesontools, pisitools
 
 j = ''.join([
     ' -Dwl-wl=true',
@@ -25,4 +24,3 @@ def install():
 	mesontools.install()
 
 	pisitools.dodoc("AUTHORS", "NEWS")
-
--- desktop/enlightenment/base/terminology/actions.py
+++ desktop/enlightenment/base/terminology/actions.py
@@ -4,8 +4,7 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import mesontools
-from pisi.actionsapi import pisitools
+from pisi.actionsapi import mesontools, pisitools
 
 def setup():
 	mesontools.configure()
@@ -17,4 +16,3 @@ def install():
 	mesontools.install()
 
 	pisitools.dodoc("AUTHORS", "NEWS")
-

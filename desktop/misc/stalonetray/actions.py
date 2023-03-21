d32d0d7c398a7d21b9894b7a9dda77828cd63e28
4fury-c3440d8 <uglyside@yandex.ru>
2023-03-21 20:03:49 +0300

    stalonetray.

--- desktop/misc/stalonetray/actions.py
+++ desktop/misc/stalonetray/actions.py
@@ -4,9 +4,7 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import autotools, pisitools, get
 
 def setup():
 	pisitools.dosed("configure.ac", "/usr/share/sgml/docbook/stylesheet/xsl/nwalsh", "/usr/share/xml/docbook/xsl-stylesheets")
@@ -19,5 +17,5 @@ def build():
 def install():
 	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-	pisitools.dodoc("AUTHORS", "NEWS", "stalonetrayrc.sample.in")
-
+	pisitools.insinto("/usr/share/doc/stalonetray", "stalonetrayrc.sample.in", "stalonetrayrc")
+	pisitools.dodoc("AUTHORS", "NEWS")
